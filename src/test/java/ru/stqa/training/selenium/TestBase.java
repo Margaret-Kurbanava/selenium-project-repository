01d2abad16807acdf883ddc68411cff487cac7f8
Margaret-Kurbanava <63010566+Margaret-Kurbanava@users.noreply.github.com>
2020-12-17 12:40:41 +0100

    Lesson 2 Task1

--- src/test/java/ru/stqa/training/selenium/TestBase.java
+++ src/test/java/ru/stqa/training/selenium/TestBase.java
@@ -9,3 +9,24 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+public class TestBase {
+
+    public WebDriver driver;
+
+    @Before
+    public  void SetUp()  {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\MKU\\seleniumlearning\\resources\\chromedriver.exe");
+        //fist part- just copy as is, second part- specify path to chromedriver
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+
+
+
+    @After
+    public void stop() {
+        driver.quit();
+    }
+}
