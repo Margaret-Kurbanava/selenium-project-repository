2a041132126bf2a06c90697ae1e9738f57a194a3
Margaret-Kurbanava <63010566+Margaret-Kurbanava@users.noreply.github.com>
2020-12-20 19:33:07 +0100

    Admin Login Test

---
+++ src/test/java/ru/stqa/training/selenium/TestAdminLogin.java
@@ -0,0 +1,22 @@
+package ru.stqa.training.selenium;
+
+import org.junit.Test;
+import pages.AdminLoginPage;
+
+public class TestAdminLogin extends TestBase{
+
+
+
+
+
+@Test
+    public void testAdminLogin (){
+
+    AdminLoginPage adminLoginPage = new AdminLoginPage(driver);
+        adminLoginPage.openAdminLoginPage();
+        adminLoginPage.enterUsename("admin");
+        adminLoginPage.enterPassword("admin");
+        adminLoginPage.clickLogin();
+
+    }
+}
--- src/test/java/ru/stqa/training/selenium/TestBase.java
+++ src/test/java/ru/stqa/training/selenium/TestBase.java
@@ -5,9 +5,12 @@ import org.junit.Before;
 import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public class TestBase {
 
@@ -16,9 +19,22 @@ public class TestBase {
     @Before
     public  void SetUp()  {
 
+        //Capabilities -dismiss unexpected browser alerts
+        ChromeOptions caps = new ChromeOptions();
+        caps.setCapability("unexpectedAlertBehaviour", "dismiss");
 
-        driver = new ChromeDriver();
+        // DesiredCapabilities caps = new DesiredCapabilities();
+        // caps.setCapability("unexpectedAlertBehaviour", "dismiss");
+
+        //driver constructor
+        driver = new ChromeDriver(caps);
+        //timeout
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //maximize window
         driver.manage().window().maximize();
+
+        //print all Capabilities
+        System.out.println(((HasCapabilities) driver).getCapabilities());
     }
 
 
@@ -26,6 +42,6 @@ public class TestBase {
 
     @After
     public void stop() {
-        driver.quit();
+        //driver.quit();
     }
 }
