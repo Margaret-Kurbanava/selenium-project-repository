2409ecaa1d70ef87e1df8e717ca2951ea8ef0d9c
Margaret-Kurbanava <63010566+Margaret-Kurbanava@users.noreply.github.com>
2020-12-20 19:37:48 +0100

    Task 3 Admin Login Test

--- src/test/java/pages/AdminLoginPage.java
+++ src/test/java/pages/AdminLoginPage.java
@@ -4,7 +4,33 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class AdminLoginPage {
+    private WebDriver driver;
 
+
+    private String adminLoginPage = "http://localhost/litecart/admin/login.php";
+    private By usernameField = By.name("username");
+    private By passwordField = By.name("password");
+    private By loginButton = By.name("login");
+
+
+
+    public AdminLoginPage(WebDriver driver) {this.driver= driver;}
+
+    public void openAdminLoginPage(){
+        driver.get(adminLoginPage);
+    }
+
+    public void enterUsename(String username){
+        driver.findElement(usernameField).sendKeys(username);
+    }
+
+    public void enterPassword(String password){
+        driver.findElement(passwordField).sendKeys(password);
+    }
+
+    public void clickLogin(){
+        driver.findElement(loginButton).click();
+    }
 
 
 }
--- src/test/java/ru/stqa/training/selenium/TestAdminLogin.java
+++ src/test/java/ru/stqa/training/selenium/TestAdminLogin.java
@@ -5,7 +5,16 @@ import pages.AdminLoginPage;
 
 public class TestAdminLogin extends TestBase{
 
+    @Test
+    public void testAdminLogin (){
 
+        AdminLoginPage adminLoginPage = new AdminLoginPage(driver);
+        adminLoginPage.openAdminLoginPage();
+        adminLoginPage.enterUsename("admin");
+        adminLoginPage.enterPassword("admin");
+        adminLoginPage.clickLogin();
+
+    }
 
 
 
