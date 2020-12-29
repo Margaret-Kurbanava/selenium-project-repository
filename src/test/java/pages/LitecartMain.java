790a5e6792a83584774d7d662f7aaa4f70c5defc
Margaret-Kurbanava <63010566+Margaret-Kurbanava@users.noreply.github.com>
2020-12-29 11:29:45 +0100

    Stickers Test Corrected

--- src/test/java/pages/LitecartMain.java
+++ src/test/java/pages/LitecartMain.java
@@ -1,17 +1,21 @@
 package pages;
 
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class LitecartMain {
 
     private WebDriver driver;
 
     private By sticker = By.cssSelector(".sticker");
-    private By product = By.cssSelector(".product.column.shadow.hover-light");
+    private By product = By.cssSelector(".product");
    private String litecartMainLink= "http://localhost/litecart/en/";
 
 
@@ -30,8 +34,9 @@ public void checkStickers(){
     List<WebElement> products = driver.findElements(product);
     for (int i = 0; i < products.size(); i++) {
 
-
-     products.get(i).findElement(sticker).isDisplayed();
+        products.get(i).findElement(sticker).isDisplayed();
+        List<WebElement> stickers= products.get(i).findElements(sticker);
+        assertEquals(1, stickers.size());
     }
     }
 
